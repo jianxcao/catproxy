59c542a56099e774086c7510aee32b3e82bae07e
jxcao <jxcao@corp.netease.com>
2019-11-01 15:40:06 +0800

    feat: ca生成改2048

--- src/cert/createCert.js
+++ src/cert/createCert.js
@@ -1,59 +1,68 @@
-import forge, {pki, md, pkcs12} from 'node-forge';
-let attrs  = [{
-	name: 'countryName',
-	value: 'CN'
-}, {
-	shortName: 'ST',
-	value: 'CP'
-}, {
-	name: 'localityName',
-	value: 'BJ'
-}, {
-	name: 'organizationName',
-	value: 'catproxy'
-}, {
-	shortName: 'OU',
-	value: 'CP'
-}];
+import forge, { pki, md, pkcs12 } from 'node-forge';
+let attrs = [
+	{
+		name: 'countryName',
+		value: 'CN'
+	},
+	{
+		shortName: 'ST',
+		value: 'CP'
+	},
+	{
+		name: 'localityName',
+		value: 'BJ'
+	},
+	{
+		name: 'organizationName',
+		value: 'catproxy'
+	},
+	{
+		shortName: 'OU',
+		value: 'CP'
+	}
+];
 let rootAttrs = attrs.slice(0);
 rootAttrs.push({
 	name: 'commonName',
 	value: 'catproxy'
 });
 
-
-let createKeyandCert = ()=> {
+let createKeyandCert = () => {
 	// generate a keypair and create an X.509v3 certificate
-	let keys = pki.rsa.generateKeyPair(1024);
+	let keys = pki.rsa.generateKeyPair(2048);
 	let cert = pki.createCertificate();
 	let today = new Date().getTime();
-	let tenYearMin = 10 * 365 * 24 * 60 * 60 * 1000;
+	let tenYearMin = 1 * 365 * 24 * 60 * 60 * 1000;
 	cert.publicKey = keys.publicKey;
 	cert.serialNumber = '' + new Date().getTime();
 	cert.validity.notBefore = new Date(today - tenYearMin);
 	cert.validity.notAfter = new Date(today + tenYearMin);
-	return {cert, keys};
+	return { cert, keys };
 };
 
 let createRootCert = () => {
-	let {cert, keys} = createKeyandCert();
+	let { cert, keys } = createKeyandCert();
 	cert.setSubject(rootAttrs);
 	// alternatively set subject from a csr
 	// cert.setSubject(csr.subject.attributes);
 	cert.setIssuer(rootAttrs);
-	cert.setExtensions([{
-		name: 'basicConstraints',
-		cA: true
-	}]);
+	cert.setExtensions([
+		{
+			name: 'basicConstraints',
+			cA: true
+		}
+	]);
 	cert.sign(keys.privateKey, md.sha256.create());
 	// base64-encode p12
-	let p12Asn1 = pkcs12.toPkcs12Asn1(keys.privateKey, cert, '123456', {algorithm: '3des'});
-	let p12Der = new Buffer(forge.asn1.toDer(p12Asn1).toHex(), 'hex')
+	let p12Asn1 = pkcs12.toPkcs12Asn1(keys.privateKey, cert, '123456', {
+		algorithm: '3des'
+	});
+	let p12Der = new Buffer(forge.asn1.toDer(p12Asn1).toHex(), 'hex');
 	return {
 		cert: pki.certificateToPem(cert),
 		pfx: p12Der,
 		privateKey: pki.privateKeyToPem(keys.privateKey),
-		publicKey: pki.publicKeyToPem(keys.publicKey),
+		publicKey: pki.publicKeyToPem(keys.publicKey)
 	};
 };
 
@@ -65,47 +74,52 @@ let createSelfCert = (domains, rootOpt) => {
 		domains = [domains];
 	}
 	let rootKey = pki.privateKeyFromPem(rootOpt.privateKey);
-	let {cert, keys} = createKeyandCert();
+	let { cert, keys } = createKeyandCert();
 	// rootCert.subject.attributes
 	cert.setIssuer(rootAttrs);
 
-// ,{
-// 		name: 'subjectAltName',
-// 		altNames: domains.map(function(host) {
-// 			if (host.match(/^[\d\.]+$/)) {
-// 				return {type: 7, ip: host};
-// 			}
-// 			return {type: 2, value: host};
-// 		})
-// 	}
+	// ,{
+	// 		name: 'subjectAltName',
+	// 		altNames: domains.map(function(host) {
+	// 			if (host.match(/^[\d\.]+$/)) {
+	// 				return {type: 7, ip: host};
+	// 			}
+	// 			return {type: 2, value: host};
+	// 		})
+	// 	}
 	cert.setExtensions([
 		// 某些电脑加这个 会爆出 证书乱码问题
-	// {
-	// 	name: 'basicConstraints',
-	// 	cA: true
-	// },
-	{
-		name: 'subjectAltName',
-		altNames: domains.map(function(host) {
-			if (host.match(/^[\d\.]+$/)) {
-				return {type: 7, ip: host};
-			}
-			return {type: 2, value: host};
-		})
-	}]);
+		// {
+		// 	name: 'basicConstraints',
+		// 	cA: true
+		// },
+		{
+			name: 'subjectAltName',
+			altNames: domains.map(function(host) {
+				if (host.match(/^[\d\.]+$/)) {
+					return { type: 7, ip: host };
+				}
+				return { type: 2, value: host };
+			})
+		}
+	]);
 
-	cert.setSubject(attrs.concat([{
-		name: 'commonName',
-		value: domains[0]
-	}]));
+	cert.setSubject(
+		attrs.concat([
+			{
+				name: 'commonName',
+				value: domains[0]
+			}
+		])
+	);
 	// console.log(111111);
 	// console.log(cert.getExtensions());
 	cert.sign(rootKey, md.sha256.create());
 	return {
 		cert: pki.certificateToPem(cert),
 		privateKey: pki.privateKeyToPem(keys.privateKey),
-		publicKey: pki.publicKeyToPem(keys.publicKey),
+		publicKey: pki.publicKeyToPem(keys.publicKey)
 	};
 };
 
-export {createRootCert, createSelfCert};
+export { createRootCert, createSelfCert };
--- src/httpsProxySer.js
+++ src/httpsProxySer.js
@@ -1,59 +1,69 @@
 // 创建一个https的代理服务器
 import https from 'https';
-import {getCert} from './cert/cert.js';
-import {getPort} from './tools';
+import { getCert } from './cert/cert.js';
+import { getPort } from './tools';
 import Promise from 'promise';
 import log from './log';
 import tls from 'tls';
 import util from 'util';
-import {requestHandler, requestUpgradeHandler} from './requestSerives';
+import constants from 'constants';
+import { requestHandler, requestUpgradeHandler } from './requestSerives';
 let SNICallback = (servername, callback) => {
 	try {
-		let {privateKey: key, cert} = getCert(servername);
-		let ctx = tls.createSecureContext({key, cert});
+		let { privateKey: key, cert } = getCert(servername);
+		let ctx = tls.createSecureContext({ key, cert });
 		callback(null, ctx);
 	} catch (e) {
 		log.error(e);
 		callback(e);
 	}
 };
-export {SNICallback};
+export { SNICallback };
 export default (host, port) => {
 	if (!host) {
 		throw new Error('host is must');
 	}
-	return Promise.resolve(port)
-	.then(p => {
-		if (p) {
-			return p;
-		} else {
-			return getPort();
-		}
-	})
-	// 不支持sni的请求可能点了就没反应,SNICallback在客户端不支持的情况下，不会报错，会直接返回
-	.then(port => {
-		let {privateKey: key, cert} = getCert(host);
-		let server = https.createServer({
-			key, 
-			cert,
-			SNICallback,
-			rejectUnauthorized: false
-		}, function(req, res) {
-			if (req.headers.upgrade) {
-				return;
-			}
-			requestHandler.call(this, req, res);
-		});
+	return (
+		Promise.resolve(port)
+			.then(p => {
+				if (p) {
+					return p;
+				} else {
+					return getPort();
+				}
+			})
+			// 不支持sni的请求可能点了就没反应,SNICallback在客户端不支持的情况下，不会报错，会直接返回
+			.then(port => {
+				let { privateKey: key, cert } = getCert(host);
+				let server = https.createServer(
+					{
+						secureOptions:
+							constants.SSL_OP_NO_SSLv3 || constants.SSL_OP_NO_TLSv1,
+						key,
+						cert,
+						SNICallback,
+						rejectUnauthorized: false
+					},
+					function(req, res) {
+						if (req.headers.upgrade) {
+							return;
+						}
+						requestHandler.call(this, req, res);
+					}
+				);
 
-		server.on('upgrade', requestUpgradeHandler);
-		
-		server.on('clientError', function(err, con) {
-			log.error('clientError', err);
-		});
-		
-		server.listen(port);
-		server.on('error', err => log.error(err + 'inner https prxoy server err:' + err));
-		return {server, port};
-	})
-	.then(null, err => log.error('create https proxy server error:' + err));
+				server.on('upgrade', requestUpgradeHandler);
+
+				server.on('clientError', function(err, con) {
+					log.error('clientError', err);
+				});
+
+				server.listen(port);
+				server.on('error', err =>
+					log.error(err + 'inner https prxoy server err:' + err)
+				);
+				return { server, port };
+			})
+			.then(null, err => log.error('create https proxy server error:' + err))
+	);
 };
